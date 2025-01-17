a57e7e0d28e5d618e6518869af70082984984585
Viacheslav Kopeika <ks031088@gmail.com>
2025-01-17 18:06:03 +0500

    Init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("app/libs/versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "Fly-Fly House"
+include(":app")
